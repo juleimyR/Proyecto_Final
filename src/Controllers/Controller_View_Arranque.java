6d6c6e300e515e0976da0d4106e4b8d6eb9b49d0
JuleimyR <juleimy022@gmail.com>
2023-11-22 14:12:59 -0500

    Creación vinculos externos

--- src/Controllers/Controller_View_Arranque.java
+++ src/Controllers/Controller_View_Arranque.java
@@ -3,6 +3,7 @@ package Controllers;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -11,11 +12,13 @@ import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Modality;
 import javafx.stage.StageStyle;
 import javafx.stage.WindowEvent;
 
-public class Controller_View_Arranque implements Initializable {
+public class Controller_View_Arranque extends Application implements Initializable {
 
     @FXML
     private Button btnAdmin;
@@ -27,6 +30,22 @@ public class Controller_View_Arranque implements Initializable {
     private Button btnSalir;
     @FXML
     private Button btnMin;
+    @FXML
+    private Button Link1;
+    @FXML
+    private Button Link2;
+    @FXML
+    private Button Link3;
+    @FXML
+    private ImageView imaLink1;
+    @FXML
+    private ImageView imaLink2;
+    @FXML
+    private ImageView imaLink3;
+    @FXML
+    private Button Link4;
+    @FXML
+    private ImageView imaLink4;
 
     public Button getBtnMin() {
         return btnMin;
@@ -34,6 +53,29 @@ public class Controller_View_Arranque implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        Image imageNormal = new Image(getClass().getResourceAsStream("/Images/Icons/git1.png"));
+        Image imageHover = new Image(getClass().getResourceAsStream("/Images/Icons/git2.png"));
+
+        Link1.setOnMouseEntered(event -> imaLink1.setImage(imageHover));
+        Link1.setOnMouseExited(event -> imaLink1.setImage(imageNormal));
+
+        Image imageNormal2 = new Image(getClass().getResourceAsStream("/Images/Icons/insta1.png"));
+        Image imageHover2 = new Image(getClass().getResourceAsStream("/Images/Icons/insta2.png"));
+
+        Link2.setOnMouseEntered(event -> imaLink2.setImage(imageHover2));
+        Link2.setOnMouseExited(event -> imaLink2.setImage(imageNormal2));
+
+        Image imageNormal3 = new Image(getClass().getResourceAsStream("/Images/Icons/whats1.png"));
+        Image imageHover3 = new Image(getClass().getResourceAsStream("/Images/Icons/whats2.png"));
+
+        Link3.setOnMouseEntered(event -> imaLink3.setImage(imageHover3));
+        Link3.setOnMouseExited(event -> imaLink3.setImage(imageNormal3));
+
+        Image imageNormal4 = new Image(getClass().getResourceAsStream("/Images/Icons/face1.png"));
+        Image imageHover4 = new Image(getClass().getResourceAsStream("/Images/Icons/face2.png"));
+
+        Link4.setOnMouseEntered(event -> imaLink4.setImage(imageHover4));
+        Link4.setOnMouseExited(event -> imaLink4.setImage(imageNormal4));
 
     }
 
@@ -59,14 +101,16 @@ public class Controller_View_Arranque implements Initializable {
             stage.initModality(Modality.APPLICATION_MODAL);
             stage.initStyle(StageStyle.UTILITY);
             stage.setScene(scene);
-            stage.showAndWait();
-            
+            stage.show();
+
             stage.setOnCloseRequest((WindowEvent event) -> {
                 controller.closeWindow();
             });
 
+            closeWindow();
+
         } else if (e.getSource() == btnLogin) {
-            
+
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/Views/View_LogIn.fxml"));
             Parent root = loader.load();
 
@@ -78,8 +122,8 @@ public class Controller_View_Arranque implements Initializable {
             stage.initModality(Modality.APPLICATION_MODAL);
             stage.initStyle(StageStyle.UTILITY);
             stage.setScene(scene);
-            stage.showAndWait();
-            
+            stage.show();
+
             stage.setOnCloseRequest((WindowEvent event) -> {
                 controller.closeWindow();
             });
@@ -89,6 +133,31 @@ public class Controller_View_Arranque implements Initializable {
     private void minimizarVentana(ActionEvent event) {
         Stage currentStage = (Stage) btnMin.getScene().getWindow();
         currentStage.setIconified(true);
+    }
+
+    @FXML
+    private void irLink(ActionEvent event) {
+        if (event.getSource() == Link1) {
+
+            String url = "https://github.com/juleimyR/Proyecto_Final";
+            getHostServices().showDocument(url);
+
+        } else if (event.getSource() == Link2) {
+            String url = "";
+        } else if (event.getSource() == Link3) {
+            String url = "";
+        } else if (event.getSource() == Link4) {
+            String url = "";
+        }
+    }
+
+    public void closeWindow() {
+        Stage miStage = (Stage) btnSign_In.getScene().getWindow();
+        miStage.close();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
     }
 
 }
