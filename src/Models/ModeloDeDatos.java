25043b8d247f2c355e9b98d66f055fa6e4cf92f0
JuleimyR <juleimy022@gmail.com>
2023-11-22 02:50:48 -0500

    Creación de Modelo de datos

---
+++ src/Models/ModeloDeDatos.java
@@ -0,0 +1,39 @@
+package Models;
+
+import Models.EstructuraDeDatos.ListaDobleAdmin;
+import Models.EstructuraDeDatos.ListaDobleCliente;
+import Models.EstructuraDeDatos.PilaStack_Producto;
+
+public class ModeloDeDatos {
+
+    private static ModeloDeDatos instancia;
+    private ListaDobleAdmin listaA;
+    private ListaDobleCliente listaC;
+    private PilaStack_Producto pilaP;
+
+    public ModeloDeDatos() {
+        listaA = new ListaDobleAdmin();
+        listaC = new ListaDobleCliente();
+        pilaP = new PilaStack_Producto();
+    }
+
+    public static ModeloDeDatos obtenerInstancia() {
+        if (instancia == null) {
+            instancia = new ModeloDeDatos();
+        }
+        return instancia;
+    }
+
+    public ListaDobleAdmin getListaA() {
+        return listaA;
+    }
+
+    public ListaDobleCliente getListaC() {
+        return listaC;
+    }
+
+    public PilaStack_Producto getPilaP() {
+        return pilaP;
+    }
+
+}
